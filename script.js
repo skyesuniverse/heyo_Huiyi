87a940c68f3939122584c5d7a64ac618cc1ed2bc
HUIYI <130475414+skyesuniverse@users.noreply.github.com>
2024-05-23 22:38:20 +0800

    update portfolio section

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 const filterButtons = document.querySelectorAll(".filter_buttons button");
-const filterableCards = document.querySelectorAll(".filterable_cards .w3-card");
+const filterableCards = document.querySelectorAll(".filter-item");
 
-console.log(filterButtons, filterableCards);
-
-const filterCards = e => {
+const filterCards = (e) => {
     document.querySelector(".active").classList.remove("active");
     e.target.classList.add("active");
-    // console.log(e.target);
 
-    filterableCards.forEach(card => {
-        card.classList.add("hide");
-        if (card.dataset.name === e.target.dataset.name || e.target.dataset.name === "all") {
+    const filterName = e.target.dataset.name;
+
+    filterableCards.forEach((card) => {
+        if (filterName === "all" || card.dataset.name === filterName) {
             card.classList.remove("hide");
+        } else {
+            card.classList.add("hide");
         }
     });
-
 };
 
-filterButtons.forEach(button => button.addEventListener("click", filterCards));
+filterButtons.forEach((button) => button.addEventListener("click", filterCards));
